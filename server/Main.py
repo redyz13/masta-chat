6b97c0d5a3712c3d73c80c0f7f474ae5a31ac051
Antonio <35410603+redyz13@users.noreply.github.com>
2021-09-12 18:15:17 +0200

    crate connection and main method into main.py

--- server/Main.py
+++ server/Main.py
@@ -1,4 +1,20 @@
 from Server import Server
 
+def run():
+    ()
+
+def connection():
+    server = Server(7000) # Server port
+    server.acceptClient()
+
+    print("\n[Server in esecuzione]\n")
+
+    run()
+
 if __name__ == '__main__':
-    server = Server(7000)
+    select = int(input("\nSelezionare un opzione\n\n1. Avvia Connessione\n0. Esci\n\nSelezione: "))
+
+    if select == 1:
+        connection()
+    else:
+        exit()
--- server/Server.py
+++ server/Server.py
@@ -5,10 +5,10 @@ class Server():
     def __init__(self, port):
         self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET (LAN, Internet) SOCK_STREAM (TCP)
         self.__port = port
-        self.__clientSocket
+        self.__clientSocket = None
 
     def acceptClient(self):
-        self.__server.bind(socket.gethostname(), 7000) # Bind to hostname and port
+        self.__server.bind((socket.gethostname(), self.__port)) # Bind to hostname and port
         self.__server.listen(0) # Connection queue before refusing
         
         print("\n[Connessione Avviata]\tIn attesa di un client...")
