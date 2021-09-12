103850cab51b4dfe688ea8b29e2ce56b8bd9c3fe
Antonio <35410603+redyz13@users.noreply.github.com>
2021-09-12 17:35:30 +0200

    created send and receive data methods

--- server/Server.py
+++ server/Server.py
@@ -3,9 +3,23 @@ import subprocess
 
 class Server():
     def __init__(self, port):
-        __server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET (LAN, Internet) SOCK_STREAM (TCP)
+        self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET (LAN, Internet) SOCK_STREAM (TCP)
         self.__port = port
+        self.__clientSocket
 
     def acceptClient(self):
         self.__server.bind(socket.gethostname(), 7000) # Bind to hostname and port
         self.__server.listen(0) # Connection queue before refusing
+        
+        print("\n[Connessione Avviata]\tIn attesa di un client...")
+
+        self.__clientSocket = self.__server.accept()
+
+        print(f"\n[Connessione Effettuata]\tClient: {self.__clientSocket.gethostname()} connesso")
+
+    def sendData(self, x):
+        x = str(x)
+        self.__clientSocket.send(x.encode())
+
+    def receiveData(self):
+        return self.__clientSocket.recv(1024)
