cf3986fd1ac442e70841393fe28341eb86c2c899
Antonio <35410603+redyz13@users.noreply.github.com>
2021-09-12 17:12:42 +0200

    added acceptClient method

--- server/Main.py
+++ server/Main.py
@@ -1,6 +1,4 @@
 from Server import Server
 
 if __name__ == '__main__':
-    server = Server("localhost", 7000)
-    print(server.getIp())
-    
+    server = Server(7000)
--- server/Server.py
+++ server/Server.py
@@ -2,7 +2,10 @@ import socket
 import subprocess
 
 class Server():
-    def __init__(self, ip, port):
-        self.__ip = ip
+    def __init__(self, port):
+        __server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET (LAN, Internet) SOCK_STREAM (TCP)
         self.__port = port
-        
+
+    def acceptClient(self):
+        self.__server.bind(socket.gethostname(), 7000) # Bind to hostname and port
+        self.__server.listen(0) # Connection queue before refusing
